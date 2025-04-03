2630cc876ac0a6e3d90e00c40cbbe7a11deaae51
koukou <kou.roppongi@gmail.com>
2025-04-03 14:03:27 +0900

    フォント追加

--- Assets/GameDirector.cs
+++ Assets/GameDirector.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 
 public class GameDirector : MonoBehaviour
 {
-    GameObject car;
-    GameObject flag;
-    GameObject distance;
+    //Findは処理重いので、publicにしてGameDirectorにアタッチすればいい
+    //GameObject型でなく、Transform型にできる。※これができない時もある
+    //Transformだけは、Getcompornentで取得する必要はない。（良く使われるから）
+    public Transform car;
+    public Transform flag;
+    public TextMeshProUGUI distance;
+    //GameObject flag;
+    //GameObject distance;
     // Start is called before the first frame update
     void Start()
     {
-        this.car = GameObject.Find("car");
-        this.flag = GameObject.Find("flag");
-        this.distance = GameObject.Find("distance");
     }
 
     // Update is called once per frame
     void Update()
     {
         //-1.62fは微調整。オブジェクトの中点が違う為
-        float length = this.flag.transform.position.x -
-            this.car.transform.position.x-1.62f;
-        this.distance.GetComponent<TextMeshProUGUI>().text = 
-            "Distance:" + length.ToString("F2") + "m";
+        float length = this.flag.position.x -
+            this.car.position.x-1.62f;
+        this.distance.text = 
+            "距離:" + length.ToString("F2") + "m";
     }
 }
